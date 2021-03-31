acea63d328058072ebcb758911c59b8be09300ed
VasanthSenthilkumarG <81663637+VasanthSenthilkumarG@users.noreply.github.com>
2021-03-31 08:21:54 +0530

    Add files via upload

---
+++ mathcal.js
@@ -0,0 +1,8 @@
+let cal = require('./math');
+let x = 5;
+let y = 5;
+console.log("add:"+cal.add(x,y));
+console.log("mul:"+cal.mul(x,y));
+console.log("div:"+cal.div(x,y));
+console.log("sub:"+cal.sub(x,y));
+console.log("mod:"+cal.mod(x,y));
